f6f7ddab8e22ab65e0b2dbb77064f99c141a1618
Miompolly <57joel39@gmail.com>
2024-02-18 11:39:03 +0200

    add handler

--- foodOnline_main/wsgi.py
+++ foodOnline_main/wsgi.py
@@ -13,5 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodOnline_main.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+
+def handler(event, context):
+    return application(event, context)
 
