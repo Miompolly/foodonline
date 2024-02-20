7ca1b1218ecce2865489cd28f306dc81f5418854
Miompolly <57joel39@gmail.com>
2024-02-20 17:30:23 +0200

    userprofile

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 
 class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'username', 'phone_number', 'role', 'is_active')
+    ordering=('-date_joined',)
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -76,26 +76,26 @@ WSGI_APPLICATION = 'foodOnline_main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.mgpstpykufcvyhtxjyza',
-        'PASSWORD': 'Yawhey@123!',  
-        'HOST': 'aws-0-eu-central-1.pooler.supabase.com',  # Update this path
-        'PORT': '5432',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'foodonline',
-#         'USER': 'root',
-#         'PASSWORD': '',  
-#         'HOST': '/opt/lampp/var/mysql/mysql.sock',  # Update this path
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres.mgpstpykufcvyhtxjyza',
+#         'PASSWORD': 'Yawhey@123!',  
+#         'HOST': 'aws-0-eu-central-1.pooler.supabase.com',  # Update this path
+#         'PORT': '5432',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'foodonline',
+        'USER': 'root',
+        'PASSWORD': '',  
+        'HOST': '/opt/lampp/var/mysql/mysql.sock',  # Update this path
+        'PORT': '3306',
+    }
+}
 
 
 AUTH_USER_MODEL='accounts.User'
