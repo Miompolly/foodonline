9576ddd13408a4bde00f1a7a3b258078f498bbe3
Miompolly <57joel39@gmail.com>
2024-02-20 16:32:25 +0200

    create a user

---
+++ accounts/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'username', 'phone_number', 'role', 'is_active')
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(User, CustomUserAdmin)
--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -96,6 +97,8 @@ DATABASES = {
 #     }
 # }
 
+
+AUTH_USER_MODEL='accounts.User'
 
 
 # Password validation
